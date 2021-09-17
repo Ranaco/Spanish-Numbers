f264d34a5bef0b0747f3670a275e6a6eec608f12
Ranaco <ranasatyamraj@gmail.com>
2021-09-17 19:40:33 +0530

    started

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  var run = MaterialApp(
+    title: 'first App',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Colors.green,
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Flutter App!',
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: Icon(
+          Icons.flutter_dash_rounded,
+          color: Colors.white,
+        ),
+        elevation: 20,
+        actions: const <Widget>[Icon(Icons.ac_unit)],
+      ),
+    ),
+  );
+  runApp(run);
+}
